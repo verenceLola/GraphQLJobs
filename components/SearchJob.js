ae756d82bedadae809833b8f64a8f94abf89bd6a
Vincent Onyango <verencelola@icloud.com>
2021-06-05 12:05:30 +0300

    - enable search on Enter

--- components/SearchJob.js
+++ components/SearchJob.js
@@ -6,13 +6,19 @@ const SearchJob = ({handleOnSearch}) => {
 
 	const handleOnChange = ({target: {value}}) => setLocation(value);
 
+	const handleOnKeyDown = ({keyCode}) => {
+		if (keyCode == 13){
+			handleOnSearch(location);
+		}
+	};
+
 	return (
 		<div className='container-fluid'>
 			<div className='text-center m-3'>
 				<p className='h5'>Enter a location to search for the available Jobs</p>
 			</div>
 			<div className='input-group input-group-sm mb-3'>
-				<input type='text' className='form-control shadow-none' onChange={handleOnChange} placeholder='Enter job location' aria-label='Enter Job Location' aria-describedby='basic-addon-1' />
+				<input type='text' className='form-control shadow-none' onKeyDown={handleOnKeyDown} onChange={handleOnChange} placeholder='Enter job location' aria-label='Enter Job Location' aria-describedby='basic-addon-1' />
 				<span className='input-group-text' onClick={() => handleOnSearch(location)} role='button' id='basic-addon-1'>Search</span>
 			</div>
 		</div>
